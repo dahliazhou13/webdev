1e6d9cb5741342ee342513257102fb081823848d
Doris Zhou <zhoudoris13@gmail.com>
2021-05-05 21:47:41 -0400

    new update

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,10 +1,17 @@
 buttons = document.querySelectorAll("button");
-
+// let songs = ["crash.mp3","kick-bass.mp3", "snare.mp3", "tom-1.mp3", "tom-2.mp3", "tom-3.mp3", "tom-4.mp3"];
 for(var i = 0; i < buttons.length; i ++){
-  buttons[i].addEventListener("click", handleClick);
+  buttons[i].addEventListener("click", function(){
+    this.style.color = "white";
+
+  });
 }
 
+function playAudio(name){
+  var audio = new Audio("sounds/"+name);
+  audio.play();
+}
 
-function handleClick(){
-  alert("clicked");
+function w(){
+
 }
