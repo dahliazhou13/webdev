a64348b9eed1af569acbbd7e63b994d2d825d4bf
Doris Zhou <zhoudoris13@gmail.com>
2021-05-17 22:23:48 -0400

    finished the drum kit web

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,36 +3,53 @@ buttons = document.querySelectorAll("button");
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function() {
 
-    switch (this.innerHTML) {
-      case "w":
-        playAudio("crash.mp3");
-        break;
-      case "a":
-        playAudio("kick-bass.mp3");
-        break;
-      case "s":
-        playAudio("snare.mp3");
-        break;
-      case "d":
-        playAudio("tom-1.mp3");
-        break;
-      case "j":
-        playAudio("tom-2.mp3");
-        break;
-      case "k":
-        playAudio("tom-3.mp3");
-        break;
-      case "l":
-        playAudio("tom-4.mp3");
-        break;
-      default:
-        playAudio("tom-4.mp3");
-    }
+    sound(this.innerHTML);
 
+    buttonAnimation(this.innerHTML);
   });
 }
 
 function playAudio(name) {
   var audio = new Audio("sounds/" + name);
   audio.play();
+}
+
+document.addEventListener("keydown", function(event) {
+
+  sound(event.key);
+  buttonAnimation(event.key);
+});
+
+function sound(name) {
+  switch (name) {
+    case "w":
+      playAudio("crash.mp3");
+      break;
+    case "a":
+      playAudio("kick-bass.mp3");
+      break;
+    case "s":
+      playAudio("snare.mp3");
+      break;
+    case "d":
+      playAudio("tom-1.mp3");
+      break;
+    case "j":
+      playAudio("tom-2.mp3");
+      break;
+    case "k":
+      playAudio("tom-3.mp3");
+      break;
+    case "l":
+      playAudio("tom-4.mp3");
+      break;
+    default:
+      playAudio("tom-4.mp3");
+  }
+}
+
+function buttonAnimation(name) {
+  var activeButton = document.querySelector("." + name);
+  activeButton.classList.toggle("pressed");
+  setTimeout(function() {activeButton.classList.toggle("pressed");}, 150);
 }
